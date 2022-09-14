7b0888233015590c167016704052800f88d0bbbc
Kelvenbango <kelvenbango@gmail.com>
2022-09-13 18:50:17 -0700

    fix functions args

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,16 @@
 
 /**
  * print_alphabet - entry point
- *
  * Description: Prints the alphabet, in lowercase, followed by new line 
- *
  * Return: void
  */
-
 void print_alphabet(void)
 {
 
 	char c = 'a';
 
-	while (c <= 'z'){
-
+	while (c <= 'z')
+	{
 		_putchar(c);
 		c++;
 	}
