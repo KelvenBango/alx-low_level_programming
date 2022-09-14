618f27d9e827bb9a7fdfcde5792e01849977c0fe
Kelvenbango <kelvenbango@gmail.com>
2022-09-13 18:30:53 -0700

    First commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,16 +11,15 @@
 int main(void)
 {
 
-	int i=0;
+	int i = 0;
 	char c;
 	char s[] = "_putchar\n";
 
-	while (i<=9)
+	while (i <= 9)
 	{
 		c = s[i];
 		_putchar(c);
 		i++;
 	}
 	return (0);
-
 }
