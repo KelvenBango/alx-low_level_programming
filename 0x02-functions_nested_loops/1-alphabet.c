cbf1b336ba7794d3ef23df571e70fd69143e1c52
Kelvenbango <kelvenbango@gmail.com>
2022-09-13 18:43:48 -0700

    First commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: Prints the alphabet, in lowercase, followed by new line 
+ *
+ * Return: void
+ */
+
+void print_alphabet(){
+
+	char c = 'a';
+	while (c <= 'z'){
+
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+}
