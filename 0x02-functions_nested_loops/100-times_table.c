40218c466dc5a15fcf44ca76c1bccd026ba58c03
Kelvenbango <kelvenbango@gmail.com>
2022-09-13 19:21:38 -0700

    Fix multiple instructions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,8 +17,11 @@ void print_times_table(int n)
 		{
 			for (b = 0; b <= n; b++)
 			{
-				c = a * b; d = c / 100; e = (c /  10) % 10;
+				c = a * b; 
+				d = c / 100;
+				e = (c /  10) % 10;
 				f = (c % 100) % 10;
+
 				if (b == 0)
 				{
 					_putchar('0');
