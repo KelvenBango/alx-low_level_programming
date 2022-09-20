8289aa6b1766794cebbb70f3d389520730cadb9e
Kelvenbango <kelvenbango@gmail.com>
2022-09-19 18:35:47 -0700

    Fix betty style code | betty style documentation

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * reset_to_98(int *n) - entry point
- * Description: function that takes a pointer to an int as parameter and updates the value 
- * 		it points to to 98.
- * Return: returns void
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: void
  */
 
 void reset_to_98(int *n)
