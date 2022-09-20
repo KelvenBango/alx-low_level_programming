724e66964397aa9d0203e2b4b867aa0f5644e3fa
Kelvenbango <kelvenbango@gmail.com>
2022-09-19 19:17:10 -0700

    Fix header

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,19 @@
  *
  * Return: void
  */
+
 void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		_putchar(*(str + i));
-		i = i + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
 	}
 	_putchar('\n');
 }
