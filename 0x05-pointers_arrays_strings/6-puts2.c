08feb196e57f399b98a8d2a18735517995906c8c
Kelvenbango <kelvenbango@gmail.com>
2022-09-19 19:14:36 -0700

    Fix betty style documentation

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,18 @@
 #include <main.h>
-
 /**
- * puts2 - entry point
- * Description : function that prints every other character of a string,
- * 		 starting with the first character, followed by a new line.
- * Return : void
+ * puts2 - prints every other character of a  string to stdout
+ * @str: string to print
+ *
+ * Return: void
  */
-
 void puts2(char *str)
 {
-	int len = strlen(str);
-        int i;
+	int i = 0;
 
-        for (i = 0; i < len ; i+=2)
-        {
-                printf("%c", *(str + i));
-        }
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i = i + 2;
+	}
+	_putchar('\n');
 }
