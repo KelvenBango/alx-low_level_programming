781b969e45ba83f13e89176d2e7e5514b926478c
Kelvenbango <kelvenbango@gmail.com>
2022-09-22 02:47:21 -0700

    Fix loop initialization

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,27 @@
 #include "main.h"
+
 /**
-*_strncpy - copies n bytes of src to dest
-*@n: limit of bytes
-*@src: string to be copied
-*@dest: string to copy to
-*
-*Return: dest
-*/
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @dest: string to copy to
+ * @src: string being copied
+ * @n: largest number of bytes to copy
+ *
+ * Return: address of dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-
-	for (i = 0; i < n && *(src + i) != '\0'; i++)
-		*(dest + 1) = *(src + i);
-
-	for (; i < n; i++)
-		*(dest + 1) = '\0';
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
