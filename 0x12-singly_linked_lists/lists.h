c2b3e31e11e08adc340a2f4a03d601f40151e9f1
Kelvenbango <kelvenbango@gmail.com>
2022-10-21 16:53:49 -0700

    Fix lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,3 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
 #include <stddef.h>
 
 /**
@@ -24,5 +21,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /* _LISTS_H_ *
+ /* _LISTS_H_ *
