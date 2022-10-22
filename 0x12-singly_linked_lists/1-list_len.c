326b3ef5fea3ff9fa91312e6cd6b1d5996bb0267
Kelvenbango <kelvenbango@gmail.com>
2022-10-21 17:04:21 -0700

    Fix code

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,11 +8,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	const list_t *tmp;
-	unsigned int i;
+    size_t nelem;
 
-	tmp = h;
-	for (i = 0; tmp; i++)
-		tmp = tmp->next;
-	return (i);
+    nelem = 0;
+    while (h != NULL)
+    {
+        h = h->next;
+        nelem++;
+    }
+    return (nelem);
 }
