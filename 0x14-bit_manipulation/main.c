c417ed2647c0f13808492e1c46779d8a3c356f91
Kelvenbango <kelvenbango@gmail.com>
2022-10-29 14:48:41 -0700

    First commit

--- 0x14-bit_manipulation/main.c
+++ 0x14-bit_manipulation/main.c
@@ -1,9 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    unsigned int n;
 
-#endif
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+    n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+    return (0);
+}
