3748955ebd26985e2d8377a52884c72716629675
Kelvenbango <kelvenbango@gmail.com>
2022-11-02 10:14:30 -0700

    wrote a program that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints it to the POSIX standard output
+ * @filename : char point which contains the pathname
+ * @letters : the number of letters it should read and print
+ *
+ * Return : return the actual number of letters it could read and print,
+ * 	    if the file can not be opened or read, return 0
+ * 	    if filename is NULL return 0
+ * 	    if write fails or does not write the expected amount of bytes, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	size_t o_stat, r_stat, w_stat;
+	char *buffer;
+
+	o_stat = open(filename, O_RDONLY);
+
+	if (filename == NULL)
+	{
+		return (0);
+		exit(1);
+	}
+
+	buffer = malloc(letters * sizeof(char));
+
+	if (buffer == NULL){
+
+		return (0);
+		exit(1);
+	}
+
+	r_stat = read(o_stat, buffer, letters);
+	w_stat = write(STDOUT, buffer, r_stat);
+
+	if (o_stat == -1 || r_stat == -1 || w_stat == -1)
+	{
+
+		free(buffer)
+		return (0);
+		exit(1);
+	}
+
+	buffer[letters] = '\0';
+
+	free(buffer);
+	close(o_stat);
+
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Headers
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*
+ * Prototypes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
