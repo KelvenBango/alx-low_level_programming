d1a75c08bad827f993e5ff9f57920a29064b847e
Kelvenbango <kelvenbango@gmail.com>
2022-11-02 14:59:15 -0700

    fix betty style documentation

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
 /**
- * read_textfile - function that reads a text file and prints it to the POSIX standard output
- * @filename : char point which contains the pathname
- * @letters : the number of letters it should read and print
+ * read_textfile - function that reads a text file and prints it to the POSIX stdout
+ * @filename : A pointer to the name of the file.
+ * @letters : the number of letters it should read an print
  *
- * Return : return the actual number of letters it could read and print,
- * 	    if the file can not be opened or read, return 0
- * 	    if filename is NULL return 0
- * 	    if write fails or does not write the expected amount of bytes, return 0
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
